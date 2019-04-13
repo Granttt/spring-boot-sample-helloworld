7c7c9e1f9bb5f435abb1481c1ca339bac46e932d
gaoxiyang <gaoxiyang1237@126.com>
2019-04-13 22:58:18 +0800

    jvm，线程池，队列练习

---
+++ src/main/java/com/designPattern/proxy/staticProxy/Star.java
@@ -0,0 +1,13 @@
+package com.designPattern.proxy.staticProxy;
+
+/**
+ * @Description: java类作用描述
+ * @Author: gaoxi
+ * @CreateDate: 2019/4/13 22:57
+ * @UpdateUser: gxy
+ * @UpdateDate: 2019/4/13 22:57
+ * @UpdateRemark: 修改内容
+ * @Version: 1.0
+ */
+public interface Star {
+}
