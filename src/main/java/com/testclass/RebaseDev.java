fa96e6b7708ee70667e4aeb5d89198c3a23d1f5f
gaoxiyang <gaoxiyang1237@126.com>
2019-12-30 15:50:24 +0800

    合并

--- src/main/java/com/testclass/RebaseDev.java
+++ src/main/java/com/testclass/RebaseDev.java
@@ -7,6 +7,6 @@ package com.testclass;
  */
 public class RebaseDev {
     public static void main(String[] args) {
-        System.out.println(11111);
+        System.out.println("1111111111111");
     }
 }
