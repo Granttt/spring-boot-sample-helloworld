eb48573f9b35088bb8d45f18932dbf05ca40e34e
gaoxiyang <gaoxiyang1237@126.com>
2020-05-28 16:21:15 +0800

    这是啥

---
+++ src/main/java/com/activities/JsonRootBean.java
@@ -0,0 +1,11 @@
+package com.activities;
+
+/**
+ * @author GXY
+ * @Package com.activities
+ * @date 2020/5/26 14:18
+ * @Copyright © 2020-2021 新流通
+ * @Description:
+ */
+public class JsonRootBean {
+}
